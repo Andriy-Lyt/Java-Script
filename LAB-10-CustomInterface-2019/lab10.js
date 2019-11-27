51d5a89916dbab236b22a1b10a1316278eb99dcc
Andriy-Lyt <55559336+Andriy-Lyt@users.noreply.github.com>
2019-11-27 15:06:33 -0800

    JS Lab 10

---
+++ LAB-10-CustomInterface-2019/lab10.js
@@ -0,0 +1,17 @@
+﻿//LAB 10-DATA STORAGE: INDEX PAGE
+//alert("home");//DELETE AFTER CONFIRMING CONNECTION
+window.onload = function () {
+var formHandle = document.forms[0];
+console.log(customerName);
+//formHandle.onsubmit = processForm;
+
+
+var customerName = document.forms[0].f_name.value;
+console.log(customerName);
+var customerColor = document.forms[0].f_color.value;
+console.log(customerColor);
+
+document.cookie = "name=customerName; color=customerColor";
+
+
+}
